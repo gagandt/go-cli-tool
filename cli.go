c3685756b32601f6a74b87b2b8bd67e0cda17eb1
GaganDeep Tomar <gagandeep280597@gmail.com>
2019-09-21 22:05:20 +0530

    Added first ls function

--- cli.go
+++ cli.go
@@ -3,13 +3,45 @@ package main
 import (
 	"fmt"
 	"os"
-	"log"
+	"log"	
+	"io/ioutil"
 	"github.com/urfave/cli"
 )
 
 func main() {
-	fmt.Println("Testing")
-	err := cli.NewApp().Run(os.Args)
+	app := cli.NewApp()
+	app.Name = "A terminal built in GoLang!"
+	app.Usage = "Provides basic functionalities like in a Terminal."
+
+	flags := []cli.Flag {
+		cli.StringFlag {
+			Name: "view-options",
+			Value: "a",
+		},
+	}
+
+
+	app.Commands = []cli.Command {
+		{
+			Name: "ls",
+			Usage: "Lists out the files and folders in the current directory.",
+			Flags: flags,
+			Action: func(c *cli.Context) error {
+				files, err := ioutil.ReadDir("./")
+				if err != nil {
+					log.Fatal(err)
+				}
+				for _, file := range(files) {
+					fmt.Println(file.Name())
+				}
+				fmt.Println(c.String("view-options") == "l");
+				return nil
+			},
+		},
+	}
+	
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
